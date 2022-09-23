baa362409598b013de9a179eea860f4c00650b46
Jackson Schuster <jschuster@microsoft.com>
2022-09-23 00:15:09 -0500

    wip8

---
+++ test/dnvm.Tests/MockHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dnvm.Tests
+{
+	internal class MockHttpClient : IClient
+	{
+		public Task<HttpResponseMessage> GetHeadersAsync(Uri uri, bool followRedirects = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DownloadArchiveAndExtractAsync(Uri uri, string archivePath, string extractPath)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetStringAsync(Uri uri)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
