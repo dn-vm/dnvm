39a7efa91904b641cd41a4a55c0dadbcb62b8b42
Andy Gocke <andy@commentout.net>
2022-10-01 23:02:19 -0700

    Fix publish script to work in CI (#13)
    Add GitVersion

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 set -e
 set -u
 
-version=`dotnet gitversion /showvariable semver`
+version=$(dotnet gitversion /showvariable semver)
 
 # On osx we always use x64 for the moment as NativeAOT doesn't support osx-arm64
 if [[ $(uname) == 'Darwin' ]]; then
@@ -13,4 +13,4 @@ else
 fi
 
 dotnet publish --sc -r $rid -c Release src/dnvm.csproj
-tar -C artifacts/bin/dnvm/Release/net7.0/$rid/publish/ -cvzf artifacts/dnvm-$version-$rid.tar.gz dnvm
+tar -C ./artifacts/bin/dnvm/Release/net7.0/$rid/publish/ -cvzf ./artifacts/dnvm-$version-$rid.tar.gz dnvm
