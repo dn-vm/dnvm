dbf0d141e6d18919492cfc585bcd7c15ec14d8b7
Andy Gocke <andy@commentout.net>
2022-11-19 18:14:17 -0800

    Allow passing a RID to publish.sh (#28)
    Lets you cross-compile for a different RID than the one you're
    running on.

--- publish.sh
+++ publish.sh
@@ -3,25 +3,46 @@
 set -e
 set -u
 
+usage() { echo "Usage: $0 [-r RID]" 1>&2; exit 1; }
+
+rid=
+while getopts ":r:" o; do
+    case "${o}" in
+        r)
+            rid=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ $# -gt 0 ]]; then
+    usage
+fi
+
 cd "$(dirname "$0")"
 
 dotnet tool restore
 
 version=$(dotnet gitversion /showvariable semver)
-if [[ $(uname) == 'Darwin' ]]; then
-    osname='osx'
-else
-    osname='linux'
-fi
+if [[ -z "${rid}" ]]; then
+    if [[ $(uname) == 'Darwin' ]]; then
+        osname='osx'
+    else
+        osname='linux'
+    fi
 
-name=$(uname -m)
-if [[ $name == 'arm64' || $name == 'aarch64' ]]; then
-    arch='arm64'
-else
-    arch='x64'
-fi
+    name=$(uname -m)
+    if [[ $name == 'arm64' || $name == 'aarch64' ]]; then
+        arch='arm64'
+    else
+        arch='x64'
+    fi
 
-rid=$osname-$arch
+    rid=$osname-$arch
+fi
 
 dotnet publish --sc -r $rid -c Release src/dnvm.csproj
 tar -C ./artifacts/bin/dnvm/Release/net7.0/$rid/publish/ -cvzf ./artifacts/dnvm-$version-$rid.tar.gz dnvm
