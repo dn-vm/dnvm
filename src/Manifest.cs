0a1a4783b5155ee916b207c433497c68ffb8037c
Andy Gocke <angocke@microsoft.com>
2022-11-20 16:14:32 -0800

    Better support for channels
    Make the install command install a channel and its latest version. This
    sets up channels to be tracked so that they can be regularly updated.

--- src/Manifest.cs
+++ src/Manifest.cs
@@ -1,22 +1,74 @@
 
+using System;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection.PortableExecutable;
 using Serde;
+using Serde.Json;
 
 namespace Dnvm;
 
-/// <summary>
-/// List of installed workloads.
-/// </summary>
-[GenerateSerde]
-[SerdeTypeOptions(MemberFormat = MemberFormat.CamelCase)]
-internal sealed partial record Manifest
+public enum Channel
 {
-    public ImmutableArray<Workload> Workloads { get; init; } = ImmutableArray<Workload>.Empty;
+    /// <summary>
+    /// Latest supported version from either the LTS or STS support channels.
+    /// </summary>
+    Latest,
+    /// <summary>
+    /// Newest LTS-supported release.
+    /// </summary>
+    Lts,
+    /// <summary>
+    /// Newest "preview" release, not including nightly builds.
+    /// </summary>
+    Preview,
+}
+
+public static partial class ManifestUtils
+{
+    public const string FileName = "dnvmManifest.json";
+    /// <summary>
+    /// Either reads a manifest in the current format, or reads a
+    /// manifest in the old format and converts it to the new format.
+    /// </summary>
+    public static Manifest? ReadNewOrOldManifest(string manifestSrc)
+    {
+        try
+        {
+            var version = JsonSerializer.Deserialize<ManifestVersionOnly>(manifestSrc).Version;
+            return version switch
+            {
+                null => JsonSerializer.Deserialize<ManifestV1>(manifestSrc).Convert(),// The first version didn't have a version field
+                2 => JsonSerializer.Deserialize<Manifest>(manifestSrc),
+                _ => null,
+            };
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    [GenerateDeserialize]
+    private partial struct ManifestVersionOnly
+    {
+        public int? Version { get; init; }
+    }
+}
+
+
+[GenerateSerde]
+public sealed partial record Manifest
+{
+    [SerdeMemberOptions(SkipDeserialize = true)]
+    public int Version => 2;
+    public required ImmutableArray<string> InstalledVersions { get; init; }
+    public required ImmutableArray<TrackedChannel> TrackedChannels { get; init; }
 }
 
 [GenerateSerde]
-[SerdeTypeOptions(MemberFormat = MemberFormat.CamelCase)]
-internal partial record struct Workload
+public partial record struct TrackedChannel
 {
-    public string Version { get; init; }
+    public Channel ChannelName { get; init; }
+    public ImmutableArray<string> InstalledVersions { get; init; }
 }
