3425f183c7ac2add05c6a6a34427b619e7acd5df
Andy Gocke <angocke@microsoft.com>
2022-11-28 00:07:55 -0800

    Move SDK install to a dotnet subdir
    Right now everything goes into the install path, wherver that may be
    (localappdata or .local/share as appropriate). This makes it hard to
    group files just by the SDK, vs what's installed by dnvm. Adding a
    subdirectory reduces confusion
    
    Also disabled global install for now. It's not clear exactly what it
    should do and probably needs more design.

--- src/DefaultConfig.cs
+++ src/DefaultConfig.cs
@@ -1,6 +1,5 @@
 
 using System.IO;
-using System.Runtime.InteropServices;
 using static System.Environment;
 
 namespace Dnvm;
@@ -9,9 +8,7 @@ public static class DefaultConfig
 {
     public const string FeedUrl = "https://dotnetcli.azureedge.net/dotnet";
 
-    internal static readonly string InstallDir = Path.Combine(GetFolderPath(SpecialFolder.LocalApplicationData, SpecialFolderOption.DoNotVerify), "dnvm");
-    internal static readonly string GlobalInstallDir =
-        Utilities.CurrentRID.OS == OSPlatform.Windows ? Path.Combine(GetFolderPath(SpecialFolder.ProgramFiles), "dotnet")
-        : Utilities.CurrentRID.OS == OSPlatform.OSX ? "/usr/local/share/dotnet" // MacOS no longer lets anyone mess with /usr/share, even as root
-        : "/usr/share/dotnet";
+    public static readonly string InstallDir = Path.Combine(
+        GetFolderPath(SpecialFolder.LocalApplicationData, SpecialFolderOption.DoNotVerify),
+        "dnvm");
 }
