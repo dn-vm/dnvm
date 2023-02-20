179f249d9882e5a8d26f793283a6c4206f797a79
Andy Gocke <andy@commentout.net>
2023-02-19 20:10:04 -0800

    Rework self-update
    Currently self-update is something done entirely by the originating
    binary -- it checks for available updates, downloads the updaate, and
    swaps the new binary in for the old.
    
    This means that the new binary doesn't have a chance to run any program
    logic.
    
    The new model is to download the new binary, and then run it in an
    installer mode. This will let the binary install itself, running
    whatever's necessary to do so.

--- src/dnvm/CommandLineOptions.cs
+++ src/dnvm/CommandLineOptions.cs
@@ -32,6 +32,12 @@ public abstract record CommandArguments
         /// or environment variables.
         /// </summary>
         public bool UpdateUserEnvironment { get; init; } = true;
+
+        /// <summary>
+        /// Only valid for self-install. Indicates that this is an update to
+        /// an existing dnvm installation.
+        /// </summary>
+        public bool Update {get; init; } = false;
     }
 
     public sealed record UpdateArguments : CommandArguments
@@ -71,24 +77,32 @@ sealed record class CommandLineArguments(CommandArguments Command)
                 bool yes = false;
                 bool prereqs = default;
                 string? feedUrl = default;
+                bool selfUpdate = false;
                 syntax.DefineOption("v|verbose", ref verbose, "Print debugging messages to the console.");
                 syntax.DefineOption("f|force", ref force, "Force install the given SDK, even if already installed");
                 syntax.DefineOption("self", ref self, "Install dnvm itself into the target location");
                 syntax.DefineOption("y", ref yes, "Answer yes to every question (or accept default).");
                 syntax.DefineOption("prereqs", ref prereqs, "Print prereqs for dotnet on Ubuntu");
                 syntax.DefineOption("feed-url", ref feedUrl, $"Set the feed URL to download the SDK from.");
+                syntax.DefineOption("update", ref selfUpdate, "[internal] Update the dnvm installation in the current location. Only intended to be called from dnvm.");
                 syntax.DefineParameter("channel", ref channel, c =>
-                {
-                    if (Enum.TryParse<Channel>(c, ignoreCase: true, out var result))
                     {
-                        return result;
-                    }
-                    var sep = Environment.NewLine + "\t- ";
-                    throw new FormatException(
-                        "Channel must be one of:"
-                        + sep + string.Join(sep, Enum.GetNames<Channel>()));
-                },
-                $"Download from the channel specified. Defaults to '{channel.ToString().ToLowerInvariant()}'.");
+                        if (Enum.TryParse<Channel>(c, ignoreCase: true, out var result))
+                        {
+                            return result;
+                        }
+                        var sep = Environment.NewLine + "\t- ";
+                        throw new FormatException(
+                            "Channel must be one of:"
+                            + sep + string.Join(sep, Enum.GetNames<Channel>()));
+                    },
+                    $"Download from the channel specified. Defaults to '{channel.ToString().ToLowerInvariant()}'.");
+
+                if (selfUpdate && !self)
+                {
+                    throw new FormatException("The --update option can only be used with --self");
+                }
+
                 command = new CommandArguments.InstallArguments
                 {
                     Channel = channel,
@@ -98,6 +112,7 @@ sealed record class CommandLineArguments(CommandArguments Command)
                     Yes = yes,
                     Prereqs = prereqs,
                     FeedUrl = feedUrl,
+                    Update = selfUpdate,
                 };
             }
 
