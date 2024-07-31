ead2336d7261831dd9df625a7607382bc1e9b1d8
Andy Gocke <andy@commentout.net>
2024-07-31 08:15:17 -0700

    Add doc comment for SdkDirNameProxy

--- src/dnvm/SerdeWraps/SdkDirNameProxy.cs
+++ src/dnvm/SerdeWraps/SdkDirNameProxy.cs
@@ -3,15 +3,15 @@ using Serde;
 
 namespace Dnvm;
 
+/// <summary>
+/// Serializes the <see cref="SdkDirName.Name"/> directly as a string.
+/// </summary>
 internal sealed class SdkDirNameProxy : ISerialize<SdkDirName>, IDeserialize<SdkDirName>
 {
     public static ISerdeInfo SerdeInfo { get; } = Serde.SerdeInfo.MakePrimitive(nameof(SdkDirName));
 
     public static SdkDirName Deserialize(IDeserializer deserializer)
-    {
-        var str = StringWrap.Deserialize(deserializer);
-        return new SdkDirName(str);
-    }
+        => new SdkDirName(StringWrap.Deserialize(deserializer));
 
     public void Serialize(SdkDirName value, ISerializer serializer)
     {
